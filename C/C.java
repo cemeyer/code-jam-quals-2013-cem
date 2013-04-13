c65b3bc3a6d8a8967ff6dca397013005ad3c17a5
Conrad Meyer <cse.cem@gmail.com>
2013-04-13 14:12:36 -0700

    C: brute-force kludge

--- C/C.java
+++ C/C.java
@@ -4,11 +4,13 @@ import java.util.*;
 
 public class C {
     public static boolean debug = false ;
+    public static boolean time = false ;
 
     public static void main(String[] args) {
         Scanner s = new Scanner(new BufferedInputStream(System.in));
 
         long T = new Scanner(s.nextLine()).nextLong();
+        long st = System.currentTimeMillis();
 
         for (long i = 0; i < T; i++) {
             String[] ABs = s.nextLine().split(" ");
@@ -21,12 +23,19 @@ public class C {
             if (debug)
                 System.out.println("XXX " + (1+i) + " start==== (" + A + ", " + B + ")");
             System.out.println("Case #" + (1+i) + ": " + answer(strA, strB, A, B));
+
+            if (i%100 == 0 && time) {
+                long now = System.currentTimeMillis();
+                long diff = now - st;
+
+                System.err.println("" + (1000.0 * i / diff ) + " t/sec");
+            }
         }
     }
 
-    public static String answer(String sA, String sB, BigInteger A, BigInteger B) {
+    public static String answer(String sA, String ssB, BigInteger A, BigInteger B) {
         BigInteger rA = root(sA, A, true);
-        BigInteger rB = root(sB, B, false);
+        BigInteger rB = root(ssB, B, false);
         BigInteger ans = BigInteger.ZERO;
 
         if (debug) {
@@ -34,18 +43,54 @@ public class C {
             System.out.println(rB);
         }
 
-        for (BigInteger i = rA; i.compareTo(rB) <= 0; i = i.add(BigInteger.ONE)) {
-            if (!palindrome(i))
+        char[] si = rA.toString().toCharArray();
+        char[] sB = rB.toString().toCharArray();
+
+        char[][] siP = new char[1][];
+        siP[0] = si;
+
+        boolean brk = false;
+        for (; !brk; incr10(siP)) {
+            if (Arrays.equals(siP[0], sB))
+                brk = true;
+
+            if (!palindrome(siP[0]))
                 continue;
 
+            BigInteger i = new BigInteger(new String(siP[0]));
+
             BigInteger iS = i.multiply(i);
-            if (!palindrome(iS))
+            if (!palindrome(iS.toString().toCharArray()))
                 continue;
 
             ans = ans.add(BigInteger.ONE);
         }
 
         return ans.toString();
+    }
+
+    public static void incr10(char[][] siP) {
+        char c = 1;
+        char[] si = siP[0];
+
+        for (int i = si.length-1; i >= 0; i--) {
+            char d = (char)(si[i] + c);
+            if (d > '9') {
+                si[i] = '0';
+                c = 1;
+            } else {
+                si[i] = d;
+                c = 0;
+                break;
+            }
+        }
+
+        if (c > 0) {
+            String longer = "1";
+            for (int i = 0; i < si.length; i++)
+                longer += "0";
+            siP[0] = longer.toCharArray();
+        }
     }
 
     public static BigInteger root(String s, BigInteger i, boolean ceil) {
@@ -117,10 +162,9 @@ public class C {
         return res;
     } 
 
-    static boolean palindrome(BigInteger iii) {
-        String s = iii.toString();
-        for (int i = 0; i < s.length()/2; i++)
-            if (s.charAt(i) != s.charAt(s.length() - 1 - i))
+    static boolean palindrome(char[] s) {
+        for (int i = 0; i < s.length/2; i++)
+            if (s[i] != s[s.length - 1 - i])
                 return false;
         return true;
     }
